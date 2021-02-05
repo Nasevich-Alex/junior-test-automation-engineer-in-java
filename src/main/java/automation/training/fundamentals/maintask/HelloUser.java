b7cc097f5c2236923cacfc3ac487bc292f62f447
Alex Nasevich <nasevichbsu@gmail.com>
2021-02-05 14:09:57 +0300

    Delete default constructor from HelloUser

--- src/main/java/automation/training/fundamentals/maintask/HelloUser.java
+++ src/main/java/automation/training/fundamentals/maintask/HelloUser.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class HelloUser {
-    public HelloUser() { }
-
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String userName = reader.readLine();
