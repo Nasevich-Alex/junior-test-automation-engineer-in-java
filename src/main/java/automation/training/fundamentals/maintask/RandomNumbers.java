287a44af73219991ac7d08f4f1a909572276c27e
Alex Nasevich <nasevichbsu@gmail.com>
2021-02-05 14:35:45 +0300

    fixed RandomNumbers class

--- src/main/java/automation/training/fundamentals/maintask/RandomNumbers.java
+++ src/main/java/automation/training/fundamentals/maintask/RandomNumbers.java
@@ -1,13 +1,23 @@
 package automation.training.fundamentals.maintask;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Random;
+
 public class RandomNumbers {
-    public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        for (int x: array) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int countOfNumbers = Integer.parseInt(reader.readLine());
+        Random random = new Random(47);
+
+        for (int i = 0; i < countOfNumbers; i++) {
+            int x = random.nextInt(25);
             System.out.println(x);
         }
-        for (int x : array) {
-            System.out.print(x);
+        for (int i = 0; i < countOfNumbers; i++) {
+            int x = random.nextInt(25);
+            System.out.print(x + " ");
         }
     }
 }
