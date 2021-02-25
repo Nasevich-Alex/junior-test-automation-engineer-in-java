038079584d2f87a6feef7cc4ccc26c943b2b55e1
Alex Nasevich <nasevichbsu@gmail.com>
2021-02-25 12:31:14 +0300

    add last exception

---
+++ src/main/java/automation/training/exceptionsAndErrors/exceptions/IllegalMarkException.java
@@ -0,0 +1,18 @@
+package automation.training.exceptionsAndErrors.exceptions;
+
+public class IllegalMarkException extends Exception {
+    public IllegalMarkException() {
+    }
+
+    public IllegalMarkException(String message) {
+        super(message);
+    }
+
+    public IllegalMarkException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public IllegalMarkException(Throwable cause) {
+        super(cause);
+    }
+}
