1485ad3f77f43ba82566fe68d0a70fa30f2e7a6e
Alex Nasevich <nasevichbsu@gmail.com>
2021-03-30 19:22:50 +0300

    add Main class

--- src/main/java/automation/training/threads/Main.java
+++ src/main/java/automation/training/threads/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         Semaphore semaphore = new Semaphore(5);
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 15; i++) {
             new Car(semaphore, i, (int) (Math.random() * 1000), (int) (Math.random() * 100)).start();
         }
     }
